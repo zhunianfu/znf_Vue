8a83e6737500c7cf5f1e64cd2fd7ca838212e19b
znfthink <m13477032471@163.com>
2021-01-10 17:06:34 +0800

    1-3

--- myapp/src/router/index.js
+++ myapp/src/router/index.js
@@ -5,15 +5,7 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
     
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
